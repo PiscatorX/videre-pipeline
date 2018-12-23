eccb8beaff9a4ff7f0877ef80b61bc3b19188286
Andrew Ndhlovu <drewxdvst@outlook.com>
2018-12-23 15:39:57 +0200

    a metahit wrapper for grouping reads

---
+++ megahit_pairs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import glob
+
+
+class  MetaHitPairs(object):
+    
+    def  __init__(self, fglob, rglob):
+        
+        parser = argparse.ArgumentParser("wrapper for megahit grouping forward and left reads")
+        parser.add_argument('qiime_summary')
+        parser.add_argument('-f','--fwd_glob', help = "wildcard for forward reads")
+        parser.add_argument('-r','--rev_glob', help = "wildcard for reverse reads")
+        parser.add_argument('-c','--cmd', help = "commands for megahit")
+        args = parser.parse_args()
+        self.args = args
+
+    def megahit(self):
+
+
+        print(args)
+
+
+
+
+megahit =  MetaHitPairs()
+megahit.megahit()
