fe02de27c95c75736eecc96834c3144263677c5e
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-03-14 16:12:49 +0200

    added help message

--- imicrobe_mmetsp.sh
+++ imicrobe_mmetsp.sh
@@ -10,6 +10,32 @@
 data_type=$1
 data_file=$2
 
+if [ -z data_type ]
+
+then
+    echo ".cds.fa"
+    echo ".cds.fa.uproc.kegg"
+    echo ".cds.fa.uproc.kegg.annotated.tab"
+    echo ".cds.fa.uproc.pfam28"
+    echo ".cds.fa.uproc.pfam28.annotated.tab"
+    echo ".cds.json"
+    echo ".fastq.tar"
+    echo ".nt.fa"
+    echo ".pep.fa"
+    echo "_clean.fasta"
+exit 1
+
+fi
+
+if [ -z data_file ]
+
+then
+    echo "No datafile provided!"
+    echo "provide .tsv datafile with 2 columns: Sample_id mmetsp_id"
+fi
+
+
+
 while read sample_id mmetsp_id
 do
 
@@ -17,15 +43,16 @@ do
   
 done <  $2
 
-  # MMETSP0982-Undescribed-sp-CCMP2436.1.fastq
-  # MMETSP0982-Undescribed-sp-CCMP2436.2.fastq
-  # MMETSP0982.cds.fa
-  # MMETSP0982.cds.fa.uproc.kegg
-  # MMETSP0982.cds.fa.uproc.kegg.annotated.tab
-  # MMETSP0982.cds.fa.uproc.pfam28
-  # MMETSP0982.cds.fa.uproc.pfam28.annotated.tab
-  # MMETSP0982.cds.json
-  # MMETSP0982.fastq.tar
-  # MMETSP0982.nt.fa
-  # MMETSP0982.pep.fa
-  # MMETSP0982_clean.fasta
+# MMETSP0982-Undescribed-sp-CCMP2436.1.fastq
+# MMETSP0982-Undescribed-sp-CCMP2436.2.fastq
+# MMETSP0982.cds.fa
+# MMETSP0982.cds.fa.uproc.kegg
+# MMETSP0982.cds.fa.uproc.kegg.annotated.tab
+# MMETSP0982.cds.fa.uproc.pfam28
+# MMETSP0982.cds.fa.uproc.pfam28.annotated.tab
+# MMETSP0982.cds.json
+# MMETSP0982.fastq.tar
+# MMETSP0982.nt.fa
+# MMETSP0982.pep.fa
+# MMETSP0982_clean.fasta
+
