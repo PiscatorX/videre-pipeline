4842c13db019d12bb4b3960f0a82f3f53613fcf1
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-03-31 11:44:29 +0200

    add insert tables for blast output

--- bin/db_connect.py
+++ bin/db_connect.py
@@ -82,164 +82,58 @@ class DB_Connect(object):
         length INT)"""
         self.conx.execute(contigs_table)
 
+        
+    def init_blastTables(self):
+
+        hit_ids = """CREATE TABLE hit_ids(
+                     accession VARCHAR(20)  NOT NULL,
+                     DNA_END INTNOT NULL,
+                     DNA_ID VARCHAR(20)  NOT NULL,
+                     DNA_ORIENTATION VARCHAR(1) NOT NULL,
+                     DNA_START INT   NOT NULL,
+                     LENGTH INT NOT NULL,
+                     ORGANISM VARCHAR,
+                     TAXON_ID INT NOT NULL)"""
+
+        queries = """CREATE TABLE queries(
+                     contig VARCHAR(20),
+                     description VARCHAR NOT NULL,
+                     length INT NOT NULL,
+                     FOREIGN KEY(contig) REFERENCES contigs(id))"""
+
+        
+        hsps ="""CREATE TABLE hsps(
+                 hit_id VARCHAR(20), 
+                 align_length	INT NOT NULL,
+                 bits	REAL NOT NULL,
+                 expect	REAL NOT NULL,
+                 frame	VARCHAR NOT NULL,
+                 gaps	INT NOT NULL,
+                 identities	INT NOT NULL,
+                 match	VARCHAR NOT NULL, 
+                 mismatches	INT NOT NULL,
+                 num_alignments	INT NOT NULL,
+                 perc_id	INT NOT NULL, 
+                 positives	INT NOT NULL,
+                 query	VARCHAR NOT NULL,
+                 query_end	INT NOT NULL,
+                 query_start	INT NOT NULL,
+                 sbjct	VARCHAR NOT NULL,
+                 sbjct_end	INT NOT NULL, 
+                 sbjct_start	INT NOT NULL,
+                 score	REAL NOT NULL,
+                 strand	VARCHAR NOT NULL,
+                 FOREIGN KEY(hit_id) REFERENCES hit_ids(accession))"""
+
+        
+        for sql_cmd in [hit_ids, queries, hsps]:
+            self.conx.execute(sql_cmd)
+
+
 
         
 if __name__ == '__main__':
     db_connect = DB_Connect()
-    db_connect.get_contig()
-
-
-
-
-
-
-#         TABLES = {        
-#         TABLES['primers'] = (
-#              " CREATE TABLE `primers` ("
-#              " `gene` CHAR(255) NOT NULL,"
-#              " `Fwd_id`  CHAR(30) NOT NULL,"
-#              " `Fwd_Primer` CHAR(255) NOT NULL,"
-#              " `Rev_id` CHAR(30) NOT NULL,"
-#              " `Rev_Primer` CHAR(255) NOT NULL,"
-#              " `Amplicon_length` CHAR(255),"
-#              " `technology` CHAR(255),"
-#              " `Reference`  CHAR(255),"
-#              " `Cross_ref`  CHAR(255),"
-#              " `notes` LONGTEXT,"
-#              " primary key (`Fwd_id`, `Rev_id`)"
-#              " ) ENGINE=InnoDB")
-        
-        
-#         TABLES['primerprop'] = (
-#              " CREATE TABLE `primerprop` ("
-#              " `primer` CHAR(30) PRIMARY KEY,"
-#              " `TmProd` LONGTEXT NOT NULL," 
-#              " `DeltaS` LONGTEXT NOT NULL," 
-#              " `Length` LONGTEXT NOT NULL," 
-#              " `GC`     LONGTEXT NOT NULL," 
-#              " `DeltaG` LONGTEXT NOT NULL," 
-#              " `DeltaH` LONGTEXT NOT NULL," 
-#              " `Tm`     LONGTEXT NOT NULL"
-#              " ) ENGINE=InnoDB")
-
-#         TABLES['amplicons'] = (
-#              " CREATE TABLE `amplicons` ("
-#              " `primer_ID` CHAR(255) NOT NULL,"	
-#              " `amplimer`  CHAR(255) NOT NULL,"	
-#              " `fwd`        INT NOT NULL,"	
-#              " `fwd_mis`    INT NOT NULL,"	
-#              " `rev`       INT NOT NULL,"	
-#              " `rev_mis`   INT NOT NULL,"	
-#              " `len`	   INT NOT NULL,"
-#              " `seq_id`    VARCHAR(1000) NOT NULL"
-#              " ) ENGINE=InnoDB")
-
-        
-#         TABLES['taxa_data'] = (
-#              " CREATE TABLE `taxa_data` ("
-#              " `seq_id` CHAR(30)  PRIMARY KEY NOT NULL,"
-#              " `taxonomy` LONGTEXT NOT NULL"
-#              " ) ENGINE=InnoDB")
-
-#         self.TABLES = TABLES
-#         self.DB_tables = list(TABLES.keys())
-
-
-#     def create_tables(self):
-        
-#         try:
-#             self.cnx.database = self.DB_NAME  
-#         except mysql.connector.Error as err:
-#             if err.errno == errorcode.ER_BAD_DB_ERROR:
-#                 print('Database Error')
-#                 raise Exception(err)
-            
-#         for table, ddl in list(self.TABLES.items()):
-#             try:
-#                 self.cursor.execute(ddl)
-#             except mysql.connector.Error as err:
-                
-#                 if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-#                     print('Table "{}" already exists.'.format(table))
-                    
-#                 else:
-#                     raise Exception(err)
-
-# #!/usr/bin/env python
-
-# from init_primerDB import PrimerDB
-# import mysql.connector
-# import argparse
-# import csv
-
-
-# class LoadDB(PrimerDB):
-
-#     def __init__(self):
-        
-#         super(LoadDB, self).__init__()
-#         parser = argparse.ArgumentParser(description="""Compile High-Throughput Sequencing (HTS) Primer database using a csv primer file.""",
-#         epilog='NOTE: This utility expects database to be created and cvs files to have headers')
-#         parser.add_argument('-p','--primers-file', dest='primers_file', action='store', 
-#                             required=True, type=str)
-#         parser.add_argument('-c','--custom-header', dest='headers', default=False, action='store_true', 
-#                             required=False, help="""use cvs headers (default=False). Used when order of column header is different from expected. Column/header are expected in the following order: "Fwd_id, Fwd_Primer, Rev_id, Rev_Primer, Target, gene, Amplicon_length, technology, Reference, Cross_ref, notes". Important! header reading is case sensitive, if provided headers must match the case those provided here""")
-
-#         self.args, unknown = parser.parse_known_args()
-#         self.cnx.database = self.DB_NAME  
-#         self.headers = self.args.headers     
-#         self.csv_reader = csv.reader(open(self.args.primers_file))
-#         self.table_cols = ["Fwd_id","Fwd_Primer","Rev_id","Rev_Primer",
-#                           "gene","Amplicon_length","technology",
-#                           "Reference","Cross_ref","notes"]
-        
-#     def LoadCSV(self):
-        
-#         #must skip the first line of csv file
-#         top_line = next(self.csv_reader)
-#         csv_headers = top_line if self.headers else self.table_cols
-        
-#         insert_dict = {}
-#         for row in self.csv_reader:
-#             row_dict = dict(zip(csv_headers, map(lambda w: w.strip(), row)))
-#             if self.headers:
-#                 for k in row_dict:
-#                     if k in self.table_cols:
-#                         insert_dict[k] = row_dict[k]
-#                     else:
-#                         print("column {} removed from data not inserted into DB (use -h/--help for help)".format(k))
-#                 if  insert_dict:
-#                     row_dict = insert_dict
-#                     insert_dict = {}
-#             self.DB_Insert(row_dict)
-#         self.cnx.commit()
-        
-#     def DB_Insert(self, row_dict):
-        
-#         cols = ','.join(row_dict.keys()) 
-#         values = ','.join( '"'+val.translate(None,""""'""")+'"'  for val in  row_dict.values())
-#         sql = """INSERT INTO primers ({}) VALUES ({})""".format(cols, values)
-        
-#         try:
-#             self.cursor.execute(sql)
-#         except mysql.connector.errors.IntegrityError as err:
-#             print(err)
-#             pass
-
-
-
-
-
-
-
-
-
-# LoadDB = LoadDB()
-# LoadDB.LoadCSV()
-                
-    #     primer_db = PrimerDB()
-#     primer_db.init_tables()
-#     primer_db.create_DB()
-#     primer_db.create_tables()
-
-    
+    if db_connect.contigs:
+        db_connect.get_contig()
+    db_connect.init_blastTables()
